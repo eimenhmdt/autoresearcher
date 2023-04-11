2c6c600006315d84611389720c8c409adbdeec38
Eimen Hamedat <eimen.hamedat@gmail.com>
2023-04-11 03:36:37 +0200

    Improve how papers are sorted
    The algorithm now takes similarity between paper title and research question into consideration

--- autoresearcher/data_sources/web_apis/semantic_scholar_loader.py
+++ autoresearcher/data_sources/web_apis/semantic_scholar_loader.py
@@ -1,4 +1,5 @@
 from autoresearcher.data_sources.web_apis.base_web_api_data_loader import BaseWebAPIDataLoader
+import jellyfish
 
 class SemanticScholarLoader(BaseWebAPIDataLoader):
     def __init__(self):
@@ -17,7 +18,7 @@ class SemanticScholarLoader(BaseWebAPIDataLoader):
         data = self.make_request("", params=params)
         return data.get('data', [])
 
-    def fetch_and_sort_papers(self, search_query, limit=100, top_n=20, year_range=None, keyword_combinations=None):
+    def fetch_and_sort_papers(self, search_query, limit=100, top_n=20, year_range=None, keyword_combinations=None, weight_similarity=0.5):
         papers = []
         if keyword_combinations is None:
             keyword_combinations = [search_query]
@@ -25,12 +26,19 @@ class SemanticScholarLoader(BaseWebAPIDataLoader):
         for combination in keyword_combinations:
             papers.extend(self.fetch_data(combination, limit, year_range))
 
-        sorted_papers = sorted(papers, key=lambda x: x['citationCount'], reverse=True)
-        # print only the citation count and title of the top 20 papers
+        max_citations = max(papers, key=lambda x: x['citationCount'])['citationCount']
+        
+        for paper in papers:
+            similarity = jellyfish.jaro_similarity(search_query, paper['title'])
+            normalized_citation_count = paper['citationCount'] / max_citations
+            paper['combined_score'] = (weight_similarity * similarity) + ((1 - weight_similarity) * normalized_citation_count)
+
+        sorted_papers = sorted(papers, key=lambda x: x['combined_score'], reverse=True)
 
         print("Top 20 papers:") 
         for paper in sorted_papers[:top_n]:
-            print(paper['citationCount'], paper['title'])
+            print(paper['citationCount'], paper['title'], f"Combined Score: {paper['combined_score']:.2f}")
+
         return sorted_papers[:top_n]
     
     
