ee420ed2c5e8aa4854ca73173794bfb9410865d0
Eimen Hamedat <eimen.hamedat@gmail.com>
2023-04-11 03:37:48 +0200

    Some refactoring

--- main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from autoresearcher.workflows.literature_review import literature_review
-
-research_question = "How will AI impact science?"
-researcher = literature_review.literature_review(research_question)
-researcher()
