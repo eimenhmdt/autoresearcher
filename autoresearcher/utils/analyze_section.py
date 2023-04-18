efd25610a2ad156215d7b62a9ecf88ef75bcd09d
Eimen Hamedat <eimen.hamedat@gmail.com>
2023-04-18 04:56:12 +0200

    Limit number of loaded pages to one

--- autoresearcher/utils/analyze_section.py
+++ autoresearcher/utils/analyze_section.py
@@ -2,7 +2,7 @@ from autoresearcher.llms.openai import openai_call
 from termcolor import colored
 
 
-def analyze_section(faiss_index, section_name, search_term, k=2):
+def analyze_section(faiss_index, section_name, search_term, k=1):
     print(colored(f"Analyzing {section_name}...", "cyan"))
     docs = faiss_index.similarity_search(search_term, k=k)
 
